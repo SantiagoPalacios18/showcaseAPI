4afd220487bd557ed9701a54fb4f3741e2dea024
Mustafa <mustafa.herreraet36@gmail.com>
2026-09-08 11:38:56 -0300

    me daba toc

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import {useState , useEffect } from "react"
-import axios from "axios"
 
 // El axios configurado que vamos a usar
 import api from "../api"
---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+import {useState , useEffect } from "react"
+
+
+
+
+
